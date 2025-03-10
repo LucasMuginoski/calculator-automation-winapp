82d4b609f46bdb596e0129c0f228b09e9b8211eb
Lucas <lucasmmuginoski@gmail.com>
2025-03-10 11:39:13 -0300

    first commit

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,88 @@
+import io.appium.java_client.windows.WindowsDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Calculator {
+
+    private WindowsDriver calcsession = null;
+    private WebElement calcresult = null;
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("Set Up");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+        try {
+            calcsession = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @BeforeMethod
+    public void clear(){
+        System.out.println("Clear");
+        calcsession.findElementByName("Limpar").click();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("Tear down!");
+        calcsession.quit();
+    }
+
+    @Test
+    public void addition(){
+        System.out.println("Running Addition");
+        calcsession.findElementByName("Um").click();
+        calcsession.findElementByName("Dois").click();
+        calcsession.findElementByName("Mais").click();
+        calcsession.findElementByName("Nove").click();
+        calcsession.findElementByName("Igual a").click();
+        Assert.assertEquals(getDisplayResult(), "21");
+    }
+
+    @Test
+    public void multiplication(){
+        System.out.println("Running Multiplication");
+        calcsession.findElementByName("Três").click();
+        calcsession.findElementByName("Três").click();
+        calcsession.findElementByName("Multiplicar por").click();
+        calcsession.findElementByName("Dois").click();
+        calcsession.findElementByName("Igual a").click();
+        Assert.assertEquals(getDisplayResult(), "66");
+    }
+
+    @Test
+    public void subtraction(){
+        System.out.println("Running Subtraction");
+        calcsession.findElementByName("Um").click();
+        calcsession.findElementByName("Dois").click();
+        calcsession.findElementByName("Menos").click();
+        calcsession.findElementByName("Nove").click();
+        calcsession.findElementByName("Igual a").click();
+        Assert.assertEquals(getDisplayResult(), "3");
+    }
+
+    @Test
+    public void division(){
+        System.out.println("Running Division");
+        calcsession.findElementByName("Três").click();
+        calcsession.findElementByName("Quatro").click();
+        calcsession.findElementByName("Dividir por").click();
+        calcsession.findElementByName("Dois").click();
+        calcsession.findElementByName("Igual a").click();
+        Assert.assertEquals(getDisplayResult(), "17");
+    }
+    public String getDisplayResult(){
+        calcresult = calcsession.findElementByAccessibilityId("CalculatorResults");
+        return calcresult.getText().replace("A exibição é", "").trim();
+    }
+}
